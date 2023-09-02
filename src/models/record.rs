ab66893fc3315fb78fa75f97397856cff1014674
Ian Zepp <ian.zepp@protonmail.com>
2023-09-02 08:56:11 -0400

    Reworked `Record` to support to/from flat data

--- src/models/record.rs
+++ src/models/record.rs
@@ -7,9 +7,33 @@ pub type RecordHash = HashMap<String, Value>;
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
-pub struct RecordJson {
-    pub meta: RecordHash,
+pub struct Record {
     pub data: RecordHash,
-    pub info: RecordHash,
-    pub acls: RecordHash,
+    pub meta: RecordHash,
 }
+
+pub fn to_record(flat: RecordHash) -> Record {
+    let mut data = flat.clone();
+    data.retain(|k, _| k != "id" && k != "ns" && k != "sc");
+
+    let mut meta = RecordHash::new();
+    meta.insert("id".to_string(), flat["id"].clone());
+    meta.insert("ns".to_string(), flat["ns"].clone());
+    meta.insert("sc".to_string(), flat["sc"].clone());
+
+    Record {
+        data: data,
+        meta: meta,
+    }
+}
+
+pub fn to_record_flat(record: Record) -> HashMap<String, Value> {
+    let mut flat = RecordHash::new();
+
+    flat.extend(record.data.clone());
+    flat.extend(record.meta.clone());
+
+    // Done
+    flat
+}
+
